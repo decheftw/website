6ff02ef3f5632d89818316bee0d0635272d63f39
Devon <devonc2000@gmail.com>
2020-07-13 23:21:46 -0400

    done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('starting up!');
+// change require to es6 import style
+import $ from 'jquery';
+import './style.scss'
+
+let time = 0
+$('#main').html(`You've been on this page for ${time} seconds.`);
+setInterval(function(){
+    time = time + 1;
+    $('#main').html(`You've been on this page for ${time} seconds.`);
+}, 1000);
